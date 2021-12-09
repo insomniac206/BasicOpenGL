cd8da4ae805f09a4e9ee90a2546541ecc52dff48
insomniac206 <76582283+insomniac206@users.noreply.github.com>
2021-12-09 19:40:02 +0530

    complete todo (line 111)

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,9 @@
 
 #include <iostream>
 #include <windows.h>
-#include "glad/glad.h"
+#include "GL/glew.h"
+#include "GL/gl.h"
+#include "GL/wglext.h"
 
 LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
@@ -17,18 +19,19 @@ ATOM RegisterWindowClass(HINSTANCE hInstance)
   wcex.lpfnWndProc = WindowProcedure;
   wcex.hInstance = hInstance;
   wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
-  wcex.lpszClassName = "Basic";
+  wcex.lpszClassName = L"Basic";
 
   return RegisterClassEx(&wcex);
 }
 
-int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, int nCmdShow)
 {
   RegisterWindowClass(hInstance);
 
+  // legacy GL Context Initialization
   HWND fakeWindow = CreateWindow(
-      "Basic",                           // Window class name
-      "BasicOGL",                        // Window title
+      L"Basic",                          // Window class name
+      L"BasicOGL",                       // Window title
       WS_CLIPSIBLINGS | WS_CLIPCHILDREN, // Window style
       0, 0,                              // position (x, y)
       1, 1,                              // size (width, height)
@@ -75,15 +78,27 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLin
     std::cout << "wglMakeCurrent() failed." << std::endl;
   }
 
-  if (!gladLoadGL())
+  // load necessary functions for creating new GL context
+  PFNWGLCHOOSEPIXELFORMATARBPROC wglChoosePixelFormatArb = nullptr;
+  wglChoosePixelFormatArb = reinterpret_cast<PFNWGLCHOOSEPIXELFORMATARBPROC>(wglGetProcAddress("wglChoosePixelFormatArb"));
+  if (wglChoosePixelFormatArb == nullptr)
   {
-    std::cout << "Failed to initialize opengl context." << std::endl;
+    std::cout << "wglGetProcAddress() failed." << std::endl;
     return 1;
   }
 
+  PFNWGLCREATECONTEXTATTRIBSARBPROC wglCreateContextAttribsArb = nullptr;
+  wglCreateContextAttribsArb = reinterpret_cast<PFNWGLCREATECONTEXTATTRIBSARBPROC>(wglGetProcAddress("wglCreateContextAttribsArb"));
+  if (wglCreateContextAttribsArb == nullptr)
+  {
+    std::cout << "wglGetProcAddress() failed." << std::endl;
+    return 1;
+  }
+
+  // GL 3.3 Context Initialization
   HWND Window = CreateWindow(
-      "Basic",
-      "OpenGL Window",
+      L"Basic",
+      L"OpenGL Window",
       WS_OVERLAPPEDWINDOW,
       CW_USEDEFAULT, CW_USEDEFAULT,
       CW_USEDEFAULT, CW_USEDEFAULT,
@@ -93,7 +108,71 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLin
 
   HDC WndDC = GetDC(Window);
 
-  // TODO: Use modern openGL functions to create modern context, and then render a triangle.
+  // TODO: Use modern openGL functions to create GL 3.3 context, and then render a triangle.
+  const int pixelAttribs[] = {
+      WGL_DRAW_TO_WINDOW_ARB, GL_TRUE,
+      WGL_SUPPORT_OPENGL_ARB, GL_TRUE,
+      WGL_DOUBLE_BUFFER_ARB, GL_TRUE,
+      WGL_PIXEL_TYPE_ARB, WGL_TYPE_RGBA_ARB,
+      WGL_ACCELERATION_ARB, WGL_FULL_ACCELERATION_ARB,
+      WGL_COLOR_BITS_ARB, 32,
+      WGL_ALPHA_BITS_ARB, 8,
+      WGL_DEPTH_BITS_ARB, 24,
+      WGL_STENCIL_BITS_ARB, 8,
+      WGL_SAMPLE_BUFFERS_ARB, GL_TRUE,
+      WGL_SAMPLES_ARB, 4,
+      0};
+
+  int pixelFormatID;
+  UINT numFormats;
+
+  bool status = wglChoosePixelFormatArb(WndDC, pixelAttribs, NULL, 1, &pixelFormatID, &numFormats);
+  if (status == false || numFormats == 0)
+  {
+    std::cout << "wglChoosePixelFormatArb() failed." << std::endl;
+    return 1;
+  }
+
+  PIXELFORMATDESCRIPTOR PFD;
+  DescribePixelFormat(WndDC, pixelFormatID, sizeof(PFD), &PFD);
+  SetPixelFormat(WndDC, pixelFormatID, &PFD);
+
+  int contextAttribs[] = {
+      WGL_CONTEXT_MAJOR_VERSION_ARB, 3,
+      WGL_CONTEXT_MINOR_VERSION_ARB, 3,
+      WGL_CONTEXT_PROFILE_MASK_ARB, WGL_CONTEXT_COMPATIBILITY_PROFILE_BIT_ARB,
+      0};
+
+  HGLRC RenderContext = wglCreateContextAttribsArb(WndDC, 0, contextAttribs);
+  if (RenderContext == NULL)
+  {
+    std::cout << "wglCreateContextAttribsArb() failed." << std::endl;
+    return 1;
+  }
+
+  wglMakeCurrent(NULL, NULL);
+  wglDeleteContext(fakeRC);
+  ReleaseDC(fakeWindow, fakeDC);
+  DestroyWindow(fakeWindow);
+
+  if (!wglMakeCurrent(WndDC, RenderContext))
+  {
+    std::cout << "wglMakeCurrent() failed." << std::endl;
+    return 1;
+  }
+
+  // function loader
+  if (glewInit() != GLEW_OK)
+  {
+    std::cout << "glewInit() failed." << std::endl;
+    return 1;
+  }
+
+  ShowWindow(Window, nCmdShow);
+
+  glClearColor(0.15f, 0.15f, 0.15f, 1.0f);
+  glClear(GL_COLOR_BUFFER_BIT);
+  SwapBuffers(WndDC);
 
   return 0;
 }
@@ -116,4 +195,3 @@ LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lPa
   }
   return 0;
 }
-
