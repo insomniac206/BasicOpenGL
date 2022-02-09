250bdb7922e5efecbc3660d9e4e8db401a072385
insomniac206 <harishabhinav027@gmail.com>
2022-02-09 22:49:09 +0530

    add functions for abstraction

---
+++ src/glInit.h
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <functional>
+#include <windows.h>
+#include "GL/glew.h"
+#include "GL/gl.h"
+#include "GL/wglext.h"
+
+// sets the attributes of PIXELFORMATDESCRIPTOR struct, and sets pixel format, for legacy opengl context
+void SetPFDLegacy(HDC DeviceContext);
+
+// sets the attributes of PIXELFORMATDESCRIPTOR struct, and sets pixel format, for modern opengl context
+void SetPFD(HDC DeviceContext);
+
+void CreateGlContextLegacy(HDC DeviceContext);
+
+void CreateGlContext(HDC DeviceContext, int GLVersionNumberMajor, int GLVersionNumberMinor);
+
+WNDCLASSEX CreateWindowClass(HINSTANCE hInstance, const wchar_t ClassName, function<LRESULT CALLBACK(HWND, UINT, WPARAM, LPARAM)> WindowProcedure);
+
+HWND CreateWindow(const wchar_t ClassName, const wchar_t WindowTitle, int WinPosX, int WinPosY, int wWidth, int wHeight, HINSTANCE hInstance);
+
+HGLRC CreateGLContext(HDC DeviceContext);
+
+template<typename glFuncPTR>
+glFuncPTR load_function(glFuncName);
