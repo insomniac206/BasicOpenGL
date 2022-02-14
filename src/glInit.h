ee0ac12d86ec0b5a85f0b8ecff580e79caddc308
insomniac206 <harishabhinav027@gmail.com>
2022-02-14 18:40:40 +0530

    Fixed error when calling load_function, moved definition to glInit.h

--- src/glInit.h
+++ src/glInit.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <iostream>
 #include <functional>
 #include <windows.h>
@@ -9,17 +11,27 @@
 void SetPFDLegacy(HDC DeviceContext);
 
 // sets the attributes of PIXELFORMATDESCRIPTOR struct, and sets pixel format, for modern opengl context
-void SetPFD(HDC DeviceContext);
+void SetPFD(HDC DeviceContext, PFNWGLCHOOSEPIXELFORMATARBPROC wglChoosePixelFormatArb);
 
 HGLRC CreateGlContextLegacy(HDC DeviceContext);
 
-HGLRC CreateGlContext(HDC DeviceContext, int GLVersionNumberMajor, int GLVersionNumberMinor);
+HGLRC CreateGlContext(HDC DeviceContext, int GLVersionNumberMajor, int GLVersionNumberMinor, PFNWGLCREATECONTEXTATTRIBSARBPROC wglCreateContextAttribsArb);
 
 WNDCLASSEX CreateWindowClass(HINSTANCE hInstance, const char* ClassName, WNDPROC WindowProcedure);
 
 HWND BGLCreateWindow(const char* ClassName, const char* WindowTitle, int WinPosX, int WinPosY, int wWidth, int wHeight, HINSTANCE hInstance);
 
-HGLRC CreateGLContext(HDC DeviceContext);
-
 template<typename glFuncPTR>
-glFuncPTR load_function(const char* glFuncName);
+glFuncPTR load_function(const char* glFuncName)
+{
+  glFuncPTR GLFunc = nullptr;
+  GLFunc = reinterpret_cast<glFuncPTR>(wglGetProcAddress(glFuncName));
+  if (GLFunc == nullptr)
+  {
+    std::cout << "wglGetProcAddress() failed." << std::endl;
+    std::cin.get();
+    return nullptr;
+  }
+
+  return GLFunc;
+}
